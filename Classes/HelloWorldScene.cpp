f1070dc2545d56056b44503b774abb4498a849ec
Tiny <tinysilk@hotmail.com>
2016-07-18 10:58:58 +0800

    完成clipping演示。

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -31,47 +31,30 @@ bool HelloWorld::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+    auto clippingContent = Sprite::create("griff.png");
     
-    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-                                origin.y + closeItem->getContentSize().height/2));
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
+    addChild(clippingContent);
     
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    clippingContent->setPosition(Vec2(visibleSize.width/2, visibleSize.height/2));
     
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    auto clipNode = ClippingNode::create(clippingContent);
+    
+    addChild(clipNode);
+    
+    clipNode->setInverted(false);
+    
+    clipNode->setAlphaThreshold(0.5);
+    
+    auto light = Sprite::create("light.png");
+    
+    clipNode->addChild(light);
+    
+    light->setPosition(Vec2(visibleSize.width/2 - 100, visibleSize.height/2));
+    
+    light->runAction(RepeatForever::create(Sequence::create(MoveBy::create(2.0, Vec2(200, 0)),
+                                                            MoveBy::create(0.01, Vec2(-200, 0))
+                                                            , NULL)) );
+    
     
     return true;
 }
